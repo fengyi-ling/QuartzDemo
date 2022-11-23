135160500c0e6b7fda41e8bf6ceb1c315e57061e
fengyi-tw <fengyi.ling@thoughtworks.com>
2022-11-23 22:42:20 +0800

    [Fengyi] Use configuration from app setting

--- Program.cs
+++ Program.cs
@@ -2,30 +2,24 @@ using Quartz;
 using QuartzDemo;
 
 var builder = WebApplication.CreateBuilder(args);
+// This is very important, quartz will not be about to read from appsettings without this line
+builder.Services.Configure<QuartzOptions>(builder.Configuration.GetSection("Quartz"));
 builder.Services.AddQuartz(q =>
 {
     q.UseMicrosoftDependencyInjectionJobFactory();
-    var helloJobKey = new JobKey("Hello");
+    var helloJobKey = new JobKey("HelloProgram");
     q.AddJob<HelloJob>(opts => opts.WithIdentity(helloJobKey));
     q.AddTrigger(opts => opts
         .ForJob(helloJobKey)
-        .WithIdentity("hello-trigger")
+        .WithIdentity("program-trigger")
         .WithCronSchedule("*/5 * * * * ?"));
-    q.SchedulerId = "custom-schedule";
-    q.SetProperty("quartz.jobStore.type", "Quartz.Impl.AdoJobStore.JobStoreTX, Quartz");
-    q.SetProperty("quartz.serializer.type", "json");
-    q.SetProperty("quartz.jobStore.driverDelegateType", "Quartz.Impl.AdoJobStore.StdAdoDelegate, Quartz");
-    q.SetProperty("quartz.jobStore.tablePrefix", "QRTZ_");
-    q.SetProperty("quartz.jobStore.dataSource", "myDS");
-    q.SetProperty("quartz.dataSource.myDS.connectionString",
-        "Server=localhost;Database=quartznet;User=fengyi;Pwd=placeholder");
-    q.SetProperty("quartz.dataSource.myDS.provider", "MySql");
 });
 builder.Services.AddQuartzHostedService(opt => { opt.WaitForJobsToComplete = true; });
 var webApplication = builder.Build();
 
 var schedulerFactory = webApplication.Services.GetRequiredService<ISchedulerFactory>();
 var scheduler = await schedulerFactory.GetScheduler();
-
 await scheduler.Start();
-await webApplication.RunAsync();
+
+webApplication.UseRouting();
+webApplication.Run();
