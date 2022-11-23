610826f29c64b4c92f5a0604d495e65e3472c178
fengyi-tw <fengyi.ling@thoughtworks.com>
2022-11-23 22:34:56 +0800

    [Fengyi] Add job store in database

--- Program.cs
+++ Program.cs
@@ -1,26 +1,31 @@
 using Quartz;
 using QuartzDemo;
 
-var builder = Host.CreateDefaultBuilder()
-    .ConfigureServices((_, services) =>
-    {
-        services.AddQuartz(q => { q.UseMicrosoftDependencyInjectionJobFactory(); });
-        services.AddQuartzHostedService(opt => { opt.WaitForJobsToComplete = true; });
-    }).Build();
-var schedulerFactory = builder.Services.GetRequiredService<ISchedulerFactory>();
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddQuartz(q =>
+{
+    q.UseMicrosoftDependencyInjectionJobFactory();
+    var helloJobKey = new JobKey("Hello");
+    q.AddJob<HelloJob>(opts => opts.WithIdentity(helloJobKey));
+    q.AddTrigger(opts => opts
+        .ForJob(helloJobKey)
+        .WithIdentity("hello-trigger")
+        .WithCronSchedule("*/5 * * * * ?"));
+    q.SchedulerId = "custom-schedule";
+    q.SetProperty("quartz.jobStore.type", "Quartz.Impl.AdoJobStore.JobStoreTX, Quartz");
+    q.SetProperty("quartz.serializer.type", "json");
+    q.SetProperty("quartz.jobStore.driverDelegateType", "Quartz.Impl.AdoJobStore.StdAdoDelegate, Quartz");
+    q.SetProperty("quartz.jobStore.tablePrefix", "QRTZ_");
+    q.SetProperty("quartz.jobStore.dataSource", "myDS");
+    q.SetProperty("quartz.dataSource.myDS.connectionString",
+        "Server=localhost;Database=quartznet;User=fengyi;Pwd=placeholder");
+    q.SetProperty("quartz.dataSource.myDS.provider", "MySql");
+});
+builder.Services.AddQuartzHostedService(opt => { opt.WaitForJobsToComplete = true; });
+var webApplication = builder.Build();
+
+var schedulerFactory = webApplication.Services.GetRequiredService<ISchedulerFactory>();
 var scheduler = await schedulerFactory.GetScheduler();
 
-var job = JobBuilder.Create<HelloJob>()
-    .WithIdentity("myJob", "group1")
-    .Build();
-
-var trigger = TriggerBuilder.Create()
-    .WithIdentity("myTrigger", "group1")
-    .ForJob("myJob", "group1")
-    .WithCronSchedule("*/5 * * * * ?")
-    .Build();
-
-await scheduler.ScheduleJob(job, trigger);
-
-// will block until the last running job completes
-await builder.RunAsync();
+await scheduler.Start();
+await webApplication.RunAsync();
